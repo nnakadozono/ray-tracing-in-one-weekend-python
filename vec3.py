5526b0cd56f51aa4b4a332965d82988b741e27fa
Naoyuki Nakadozono <nnakadozono@users.noreply.github.com>
2024-01-14 23:57:15 +0900

    3. add vec3

---
+++ vec3.py
@@ -0,0 +1,18 @@
+
+class Vec3():
+    def __init__(self, e0=0, e1=0, e2=0):
+        self.e = [e0, e1, e2]
+
+    def x(self):
+        return self.e[0]
+    
+    def y(self):
+        return self.e[1]
+
+    def y(self):
+        return self.e[2]
+
+    
+
+
+
