441eceded472bacbfadb32ac696113ec26955db0
Naoyuki Nakadozono <nnakadozono@users.noreply.github.com>
2024-01-15 23:57:49 +0900

    3. overload operators

--- vec3.py
+++ vec3.py
@@ -12,7 +12,17 @@ class Vec3():
     def y(self):
         return self.e[2]
 
+    def __neg__(self):
+        return Vec3(-self.e[0], -self.e[1], -self.e[2])
     
+    def __getitem__(self, num):
+        return self.e[num]
+    
+    def __add__(self, v):
+        self.e[0] += v.e[0]
+        self.e[1] += v.e[1]
+        self.e[2] += v.e[2]
+        return self
 
 
 
