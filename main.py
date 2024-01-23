dc66b30837c3701c7f8a60d2371012254f8b72bb
Naoyuki Nakadozono <nnakadozono@users.noreply.github.com>
2024-01-23 22:43:46 +0900

    4

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 import sys
 import vec3
 import color
+import ray
+
+def ray_color(r):
+    unit_direction = vec3.unit_vector(r.direction())
+    a = 0.5 * (unit_direction.y() + 1.0)
+    return (1-a) * color.Color(1.0, 1.0, 1.0) + a * color.Color(0.5, 0.7, 1.0)
 
 def main():
-    image_width = 256
-    image_height = 256
+    # Image
+    aspet_ratio = 16.0 / 9.0
+    image_width = 400
+
+    # Calculate the image height, and ensure that it's at least 1.
+    image_height = int(image_width / aspet_ratio)
+    image_height = 1 if (image_height < 1) else image_height
+
+    # Camera
+    focal_length = 1.0
+    viewport_height = 2.0
+    viewport_width = viewport_height * float(image_width / image_height)
+    camera_center = vec3.Point3(0, 0, 0)
+
+    # Calculate the vectors across the horizontal and down the vertical viewport edges.
+    viewport_u = vec3.Vec3(viewport_width, 0, 0)
+    viewport_v = vec3.Vec3(0, -viewport_height, 0)
+
+    # Calculate the horizontal and vertical delta vectors from pixel to pixel
+    pixel_delta_u = viewport_u / image_width
+    pixel_delta_v = viewport_v / image_height
+
+    # Calculate the location of the upper left pixel
+    viewport_upper_left = camera_center \
+                    - vec3.Vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2
+    pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v)
+
+    # Render
 
     print("P3")
     print(f"{image_width} {image_height}")
@@ -13,15 +45,14 @@ def main():
     for j in range(image_height):
         print(f"\rScanlines remaining: {image_height-j} ", file=sys.stderr, end="", flush=True)
         for i in range(image_width):
-            r = float(i) / (image_width - 1)
-            g = float(j) / (image_height - 1)
-            b = 0
+            pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v)
+            ray_direction = pixel_center - camera_center
+            r = ray.Ray(camera_center, ray_direction)
 
-            pixel_color = color.Color(r, g, b)
+            pixel_color = ray_color(r)
             color.write_color(pixel_color)
     
     print(f"\rDone.                    ", file=sys.stderr, flush=True)
-    
 
 if __name__ == '__main__':
     main()
--- ray.py
+++ ray.py
@@ -3,5 +3,11 @@ class Ray:
         self.__orig = origin
         self.__dir = direction
 
+    def origin(self):
+        return self.__orig
+    
+    def direction(self):
+        return self.__dir
+    
     def at(self, t):
         return self.__orig + t * self.__dir
